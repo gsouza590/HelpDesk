8dae0b363bee3c400efca554c94d5a0204eee577
gsouza590 <gsouza590@gmail.com>
2023-11-29 14:33:32 -0300

    Create profiles test dev

--- src/main/java/com/gabriel/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/gabriel/helpdesk/HelpdeskApplication.java
@@ -1,19 +1,13 @@
 package com.gabriel.helpdesk;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
+public class HelpdeskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 	}
 
 }
